0a59ecf59195ef8388e9b1fb351ee0917eb66d7d
ThisilvaSp <thisilva.sp@gmail,com>
2021-04-24 18:01:20 -0300

    Projetos MySQL

---
+++ MySQL/pizzaria/Pizza1.sql
@@ -0,0 +1,10 @@
+ CREATE TABLE tb_pizza (
+ id_Pizza tinyint,
+ nome_Pizza varchar(10),
+ borda_Pizza varchar(10), 
+ recheio_Pizza varchar(35),
+ tipo_Pizza varchar(10)
+ );
+ 
+ describe db_pizzaria_legal;
+ 
