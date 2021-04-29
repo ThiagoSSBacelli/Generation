fdebb6bf34bb89d75d2f61d54c874604bef314cc
ThisilvaSp <thisilva.sp@gmail,com>
2021-04-29 14:49:30 -0300

    Projeto 28/04

--- projetoAPI/src/main/java/com/projetoAPI/controller/PostagemController.java
+++ projetoAPI/src/main/java/com/projetoAPI/controller/PostagemController.java
@@ -3,10 +3,15 @@ package com.projetoAPI.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,4 +41,17 @@ public class PostagemController {
 		return ResponseEntity.ok(repositoty.findAllByTituContainingIgnoreCase(titulo));
 	}
 
+	@PostMapping
+	public ResponseEntity<Postagem> post(@RequestBody Postagem Postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repositoty.save(Postagem));
+	}
+
+	@PutMapping
+	public ResponseEntity<Postagem> put (@RequestBody Postagem Postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repositoty.save(Postagem));
+	}
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repositoty.deleteById(id);
+	}
 }
